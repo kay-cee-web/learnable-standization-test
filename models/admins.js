5f89f121dd8c1689e17324e641cd29db80f87bb1
ezeka valentine <ezekaval81@gmail.com>
2020-10-17 16:25:29 +0100

    validate that all required fields are filled out before sending to datas to database in admin.js

---
+++ models/admins.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const Schema = mongoose.Schema;
+const SALT_WORK_FACTOR = 10;
+
+
+
+// removing all deprecationwarning for validations
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+
+//validate that all required fields are filled out
+const AdminSchema = new Schema({
+    name:{
+        type: String,
+        required: [true, 'Name field is required']
+    },
+    email:{
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: [true, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    password:{
+            type: String,
+            required: [true, 'Password is required']
+    },
+   
+})
+
+
+//encrypting password before sending to database
+//before sending details to database
+AdminSchema.pre('save', async function save(next) {
+    if (!this.isModified('password')) return next();
+        try {
+            const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+            this.password = await bcrypt.hash(this.password, salt);
+            return next();
+        } catch (err) {
+          return next(err);
+        }
+  });
+  
+  AdminSchema.methods.validatePassword = async function validatePassword(data) {
+    return bcrypt.compare(data, this.password);
+  };
+
+const Admin = mongoose.model('admin', AdminSchema);
+
+
+module.exports = Admin;
