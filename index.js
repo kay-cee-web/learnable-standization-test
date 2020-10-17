de65a934100773828f117fb1c622eb55a540f677
ezeka valentine <ezekaval81@gmail.com>
2020-10-17 16:39:35 +0100

    added middleware for error handling  and start listening to localhost  port 3000

--- index.js
+++ index.js
@@ -14,3 +14,17 @@ app.use(bodyPaser.json());
 
 // initialize routes
 app.use('/api', router );
+
+
+
+// middleware for error handling
+app.use((err, req, res, next)=>{
+    res.status(422).send({error: err.message})
+})
+
+
+// listen for requests
+ app.listen(process.env.port || 3000, ()=>{
+     console.log('listening to loocalhost @ port 3000')
+ })
+
