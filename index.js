ba1342a92e2bec91fcf76838ee80f65859cf797c
ezeka valentine <ezekaval81@gmail.com>
2020-10-17 16:18:53 +0100

    setting up an express app, connect to mongo database and initialize routes

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const bodyPaser = require('body-parser');
 const mongoose = require('mongoose');
+
+//setting up an express app
+const app = express();
+
+//connect to mongo database
+mongoose.connect('mongodb://localhost/madamsaucedb', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.Promise = global.Promise;
+
+app.use(bodyPaser.json());
+
+
+// initialize routes
+app.use('/api', router );
