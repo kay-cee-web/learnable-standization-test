1657fe16c03e4b4502fc42b89ac44cabf5b56b14
ezeka valentine <ezekaval81@gmail.com>
2020-10-17 17:56:33 +0100

    made some litttle changes

--- index.js
+++ index.js
@@ -39,3 +39,8 @@ app.use((err, req, res, next)=>{
  // localhost:3000/api/foods -- post, get, update and delete foods
  // localhost:3000/api/avaliablefoods -- for getting all the avaliable for the day foods
  // localhost:3000/api/orders --- post, get, update and delete orders
+
+  ////// NB //////
+ //// Authomatically once u adds food to the menu, it is unavaliable to the customers
+ /// To make food avaliable for the day, select the particular food by id and change
+ /// the avaliable colum from false to true
