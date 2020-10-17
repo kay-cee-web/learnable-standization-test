a682046ccfd0084be1beee77f46cecc06644850e
ezeka valentine <ezekaval81@gmail.com>
2020-10-17 16:41:14 +0100

    addded post route -- for adding food to food menu

--- routes/app.js
+++ routes/app.js
@@ -5,3 +5,14 @@ const Food = require('../models/food');
 const Admin = require('../models/admin');
 const Order = require('../models/order');
 const User = require('../models/user');
+
+
+
+// post route -- for adding food to food menu
+router.post('/foods', (request, response, next)=>{
+    Food.create(request.body)
+        .then((food)=>{
+            response.send(food);
+         })
+        .catch(next);
+});
