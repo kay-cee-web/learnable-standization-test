6093eeb6c014adf9ff3a95693e7602e5061eb0c3
ezeka valentine <ezekaval81@gmail.com>
2020-10-17 17:08:27 +0100

    added routes for testing

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const mongoose = require('mongoose');
 const router = require('./routes/app')
 
 //setting up an express app
-const app = express();
+ const app = express();
 
-//connect to mongo database
-mongoose.connect('mongodb://localhost/madamsaucedb', {useNewUrlParser: true, useUnifiedTopology: true})
-mongoose.Promise = global.Promise;
+ //connect to mongo database
+ mongoose.connect('mongodb://localhost/madamsaucedb', {useNewUrlParser: true, useUnifiedTopology: true})
+ mongoose.Promise = global.Promise;
 
-app.use(bodyPaser.json());
+ app.use(bodyPaser.json());
 
 
-// initialize routes
+ // initialize routes
 app.use('/api', router );
 
 
@@ -29,3 +29,13 @@ app.use((err, req, res, next)=>{
      console.log('listening to loocalhost @ port 3000')
  })
 
+
+ //////////////////// PLEASE READ ///////////////////////
+ //////////////////// for our route tesing please use ///
+ // localhost:3000/api/admin -- post, get, update and delete admin
+ // localhost:3000/api/admin/login -- for admin to login
+ // localhost:3000/api/users --- post, get, update and delete users
+ // localhost:3000/api/users/login --- for a user to login
+ // localhost:3000/api/foods -- post, get, update and delete foods
+ // localhost:3000/api/avaliablefoods -- for getting all the avaliable for the day foods
+ // localhost:3000/api/orders --- post, get, update and delete orders
